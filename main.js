50fdfe32de82270b0f6a6e01e43f79d09951d98a
Frank Xavio <frank2006x@gmail.com>
2024-10-28 19:27:08 +0530

    display number

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
+function assignNumberButton(buttonId,number){
+    const button=document.getElementById(buttonId)
+    button.addEventListener('click',()=>{
+        if (active==true){
+            display.textContent=''
+            active=false
+        }
+        display.textContent = display.textContent == 0 ? number 
+        : display.textContent + number;
+    })
+}
+function numberAssigner(){
+    a=display.textContent
+
+}
+
+
 function add(a,b){
     return a+b
 }
@@ -25,4 +42,38 @@ function operate(a,b,operate){
 }
 let a
 let b
-let operator
+let active=false
+
+let display=document.getElementById('text')
+const operator={
+    '+':document.getElementById('+'),
+    '-':document.getElementById('-'),
+    '*':document.getElementById('*'),
+    '/':document.getElementById('/')
+}
+const equal=document.getElementById('=');
+const buttons=document.querySelectorAll('button')
+if (!active){
+    for (const i in operator){
+        operator[i].addEventListener('click',()=>{
+            Object.values(operator).forEach(btn => btn.style.backgroundColor = '#52c9dc');
+            active=true
+            a=display.textContent
+            operator[i].style.backgroundColor='red'
+        });
+    }
+}
+   
+
+equal.addEventListener('click',()=>{
+    b=display.textContent
+    for (const i in operator){
+        operator[i].style.backgroundColor='#52c9dc';
+    }
+    console.log(a,b)
+})
+
+for (let i =0 ; i <10 ;i++){
+    assignNumberButton(i.toString(),i)
+
+}
